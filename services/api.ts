3422693b028c15841b53b1b57ee9c5eb0e23a363
Daniel <daniel.avzaradel@gmail.com>
2025-06-22 02:47:29 +0300

    removing commented code

--- services/api.ts
+++ services/api.ts
@@ -25,17 +25,3 @@ export const fetchMovies = async({ query }: { query: string }) => {
     const data = await response.json();
     return data.results;
 }
-
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODg2NmJmNWZiNTlhNzgwZjM2MmUwZjZlYzZlYzYzNiIsIm5iZiI6MTc1MDU0ODI4MS41OTM5OTk5LCJzdWIiOiI2ODU3M2YzOTQyNzJkMWI3YjA0YmNlNjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Lwj75mQ7YQ6WcjtWGnc4Ed5LipRGOsnTHktjwSCqy2U'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
