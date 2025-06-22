ddbeccac62ade854267856e50f6e66b857c5a6e6
Daniel <daniel.avzaradel@gmail.com>
2025-06-22 03:10:48 +0300

    fixed the hook api key

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
-import { Image, ScrollView, Text, View } from "react-native";
-import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: "" }))
 
   return (
     <View className="flex-1 bg-primary">
@@ -16,12 +20,25 @@ export default function Index() {
         paddingBottom: 10
       }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for a movie"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size={"large"}
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text className="text-red-500">Error: {moviesError?.message}</Text>
+        ) :
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+            <>
+              <Text className="text-lg text-white font-bold mt-5">Latest Movies</Text>
+            </>
+          </View>
+        }
       </ScrollView>
     </View>
   );
